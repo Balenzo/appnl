ad75871d1b3847d13cc9955d77babf56e25930bb
Balenzo <nicolasmintjens@hotmail.com>
2026-05-10 23:09:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,3 @@
-const CACHE_NAME = "app-cache-v3";
-
-// Install
-self.addEventListener("install", (event) => {
-  console.log("SW geïnstalleerd");
-  self.skipWaiting();
-});
-
-// Activate
-self.addEventListener("activate", (event) => {
-  console.log("SW geactiveerd");
-
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-
-  self.clients.claim();
-});
-
-// Fetch (altijd nieuwste versie)
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener("install",e=>{
+console.log("Service Worker actief");
 });
